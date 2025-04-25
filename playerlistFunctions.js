edd4dc884fcc7bf847531bfde025f0a7a4dcc9d6
TwiSteorra <twisteorra@gmail.com>
2025-04-25 17:35:34 -0400

    updateEverything

--- playerlistFunctions.js
+++ playerlistFunctions.js
@@ -1,6 +1,35 @@
-function winners(){
-    const winner = "amogus";
-
-    document.getElementById("playerlist").innerHTML = winner;
+function compilePlayers(){
+    return document.getElementById("playerlist").value.split('\n');
 }
 
+function winners(){
+    const matchups = compilePlayers();
+    var split, newMatchups = "";
+    for(let i=0;i<matchups.length;i++){
+        split = matchups[i].split(" ");
+        if(split[0] == "(W)"){newMatchups += split[1] + "\n";}
+        else if(split[0] == "(L)"){newMatchups += split[3] + "\n";}
+    }
+    document.getElementById("compiled").value = newMatchups;
+}
+
+function losers(){
+    const matchups = compilePlayers();
+    var split, newMatchups = "";
+    for(let i=0;i<matchups.length;i++){
+        split = matchups[i].split(" ");
+        if(split[0] == "(L)"){newMatchups += split[1] + "\n";}
+        else if(split[0] == "(W)"){newMatchups += split[3] + "\n";}
+    }
+    document.getElementById("compiled").value = newMatchups;
+}
+
+function unplayed(){
+    const matchups = compilePlayers();
+    var split, newMatchups = "";
+    for(let i=0;i<matchups.length;i++){
+        split = matchups[i].split(" ");
+        if(split.length == 3){newMatchups += split[0] + " vs " + split[2] + "\n";}
+    }
+    document.getElementById("compiled").value = newMatchups;
+}
